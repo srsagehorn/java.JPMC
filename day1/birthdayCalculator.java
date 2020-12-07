cc526e4417f87fa6857e7ac201698c23cc2303f7
Shannon Sagehorn <srsagehorn@gmail.com>
2020-12-07 12:16:54 -0600

    birthday calculator

---
+++ day1/birthdayCalculator.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.time.Month;
+
+//Birthday Calculator
+
+public class Main {
+    public static void main(String args[]) {
+
+        LocalDate birthdate = LocalDate.of(1998, Month.AUGUST, 23);
+        LocalDate birthDay = birthdate.getDayOfMonth();
+        LocalDate currentDate = LocalDate.now();
+        LocalDate thisYearDay = LocalDate.getDayOfMonth(birthday);
+        Years age = Years.yearsBetween(birthdate, currentDate);
+        long daysTil = DAYS.between(birthday, currentDate);
+
+        System.out.println(age.getYears());
+        System.out.println("Your birthday is " + birthdate);
+        System.out.println("This means that you were born on a" + birthDay);
+        System.out.println("This year it falls on a" + thisYearDay);
+        System.out.println("Since today is" + currentDate);
+        System.out.println("There are" + daysTil + "your next birthday when you turn" + age + 1);
+    }
+}
